06c884a31a37a553e87cdd8999da96f7259da3ff
ldk111 <luke.david.kennedy@gmail.com>
2024-01-05 20:25:55 +0000

    adding test offsets

--- Gaussian Offset Prediction/gaussian_offset_prep.py
+++ Gaussian Offset Prediction/gaussian_offset_prep.py
@@ -6,6 +6,20 @@ def generate_offsets(axis, n = 50, sigma = 0.1):
     np.savetxt("offsets_" + axis + ".csv", offsets, delimiter = ",")
     return offsets
 
+def generate_test_offsets(axis):
+    offsets = np.zeros((12, 6))
+    if axis == "y":
+        for i in range(0, 6):
+            offsets[i,i] = 0.2
+    else:
+        for i in range(6, 12):
+            offsets[i,i-6] = 0.2
+    np.savetxt("offsets_test_" + axis + ".csv", offsets, delimiter = ",")
+    return offsets
+
 if __name__ == "__main__":
-    offsets_y = generate_offsets(axis = "y", n = 50, sigma = 0.1)
-    offsets_z = generate_offsets(axis = "z", n = 50, sigma = 0.1)
+    offsets_y = generate_offsets(axis = "y", n = 50, sigma = 0.2)
+    offsets_z = generate_offsets(axis = "z", n = 50, sigma = 0.2)
+
+    offsets_test_y = generate_test_offsets("y")
+    offsets_test_z = generate_test_offsets("z")
