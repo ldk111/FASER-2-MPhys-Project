ac427759c2a42f777b3d6f19a80f0d3bb78dbc4e
Luke Kennedy <luke.david.kennedy@gmail.com>
2024-01-30 23:49:31 +0000

    update kalman_alignedTel to work with final transformation code consistently, gaussian_offset_prep to include rotations

--- Gaussian Offset Prediction/gaussian_offset_prep.py
+++ Gaussian Offset Prediction/gaussian_offset_prep.py
@@ -1,33 +1,77 @@
 import numpy as np
 
-def generate_offsets(axis, n = 50, sigma = 0.1, outdir = ""):
+def generate_offsets(axis, n = 50, sigma = 0.0625, outdir = ""):
     offsets = np.random.normal(loc = 0, scale = sigma, size = (n, 6))
     offsets[:, 0] = 0
-    offsets[:, 3] = 0
-    offsets[:, 4] = 0
-    offsets[:, 5] = 0
+    #offsets[:, 3] = 0
+    #offsets[:, 4] = 0
+    #offsets[:, 5] = 0
     np.savetxt(outdir + "offsets_" + axis + ".csv", offsets, delimiter = ",")
+    return offsets
+
+def generate_rotations(axis, n = 50, sigma = np.pi/32, outdir = ""):
+    offsets = np.random.normal(loc = 0, scale = sigma, size = (n, 6))
+    offsets[:, 0] = 0
+    #offsets[:, 3] = 0
+    #offsets[:, 4] = 0
+    #offsets[:, 5] = 0
+    np.savetxt(outdir + "rotations_" + axis + ".csv", offsets, delimiter = ",")
     return offsets
 
 def generate_blank_offsets(axis, n = 50, outdir = ""):
     offsets = np.zeros((n, 6))
     np.savetxt(outdir + "offsets_" + axis + ".csv", offsets, delimiter = ",")
 
+def generate_blank_rotations(axis, n = 50, outdir = ""):
+    offsets = np.zeros((n, 6))
+    np.savetxt(outdir + "rotations_" + axis + ".csv", offsets, delimiter = ",")
+
 def generate_test_offsets(axis, outdir = ""):
-    offsets = np.zeros((12, 6))
-    if axis == "y":
+    offsets = np.zeros((6, 6))
+    if axis == "x":
         for i in range(0, 6):
             offsets[i,i] = 0.05
     elif axis == "z":
         for i in range(6, 12):
             offsets[i,i-6] = 0.05
-    np.savetxt(outdir + "offsets_test_" + axis + ".csv", offsets, delimiter = ",")
+    np.savetxt(outdir + "offsets_" + axis + ".csv", offsets, delimiter = ",")
+    return offsets
+
+def generate_test_rotations(axis, outdir = ""):
+    offsets = np.zeros((18, 6))
+    if axis == "x":
+        for i in range(0, 6):
+            offsets[i,i] = np.pi/8
+    elif axis == "y":
+        for i in range(6, 12):
+            offsets[i,i-6] = np.pi/8
+    elif axis == "z":
+        for i in range(12, 18):
+            offsets[i,i-12] = np.pi/8
+    np.savetxt(outdir + "rotations_" + axis + ".csv", offsets, delimiter = ",")
     return offsets
 
 if __name__ == "__main__":
-    offsets_y = generate_offsets(axis = "y", n = 50, sigma = 0.0625, outdir = "")
-    offsets_z = generate_offsets(axis = "z", n = 50, sigma = 0.0625, outdir = "")
-    offsets_x = generate_blank_offsets(axis = "x", n = 50, outdir = "")
+    #offsets_y = generate_blank_offsets(axis = "y", n = 6, outdir = "")
+    #generate_blank_offsets(axis = "x", n = 50, outdir = "")
+    #offsets_x = generate_test_offsets(axis = "x", outdir = "")
+
+    generate_rotations(axis = "x")
+    generate_rotations(axis = "y")
+    generate_rotations(axis = "z")
+
+    generate_offsets(axis = "x")
+    generate_offsets(axis = "y")
+    generate_offsets(axis = "z")
+
+    #generate_test_rotations("x")
+    #generate_test_rotations("y")
+    #generate_test_rotations("z")
+
+    #generate_blank_rotations("x", n=50)
+    #generate_blank_rotations("y", n=50)
+    #generate_blank_rotations("z", n=50)
+
 
     #offsets_test_y = generate_test_offsets("y", outdir = "variable_test/")
     #offsets_test_z = generate_test_offsets("z", outdir = "variable_test/")
--- Gaussian Offset Prediction/kalman_alignedTel.py
+++ Gaussian Offset Prediction/kalman_alignedTel.py
@@ -200,20 +200,24 @@ def runTruthTrackingKalman(
 
 def runACTS(args):
 
-    index, offset_x, offset_y, offset_z, field, digiConfigFile, trackingGeometry, inputParticlePath, rand_, outputPath = args
+    index, offset_x, offset_y, offset_z, rotation_x, rotation_y, rotation_z, field, digiConfigFile, trackingGeometry, inputPath, rand_, outputPath = args
     
     detector_misal, trackingGeometry_misal, decorators_misal = acts.examples.AlignedTelescopeDetector.create(
-            bounds=[500, 1500], positions=[10000, 10500, 11000, 19500, 20000, 20500], binValue=0, offsets=[offset_z, -offset_y, offset_x], thickness=4,rnd=rand_, 
+            bounds=[500, 1500], positions=[10000, 10500, 11000, 19500, 20000, 20500], binValue=0,
+            offsets=[offset_z, offset_y, offset_x], rotations=[rotation_z, rotation_y, rotation_x],
+            thickness=4,rnd=rand_, 
             sigmaInPlane=0.0 , sigmaOutPlane=0.0 , sigmaOutRot=0.0 , sigmaInRot=0.00 
         )
 
+    #inputParticlePath = Path(inputPath + str(index) + ".root")
+
     runTruthTrackingKalman(
-        trackingGeometry = trackingGeometry,
-        trackingGeometry_misal = trackingGeometry_misal,
+        trackingGeometry = trackingGeometry_misal,
+        trackingGeometry_misal = trackingGeometry,
         field = field,
         digiConfigFile = digiConfigFile,
         outputDir = outputPath + str(index),
-        inputParticlePath = inputParticlePath,
+        inputParticlePath = inputPath,
     ).run()
 
 def main():
@@ -226,36 +230,26 @@ def main():
 
     rand_ = random.randint(0, 50000)
 
-    outputPath = "output/110124_50_500k@200k/"
+    inputParticlePath = Path("/home/chri6112/Downloads/nmuon_acts_sample_500k.root")
+    outputPath = "output/300124_5_500k@200k/"
+    
+    digiConfigFile= "/data/atlassmallfiles/users/chri6112/Acts/acts/Examples/Algorithms/Digitization/share/default-smearing-config-telescope.json"
 
     offsets_x = np.loadtxt(outputPath + "offsets_x.csv", delimiter = ",")
     offsets_y = np.loadtxt(outputPath + "offsets_y.csv", delimiter = ",")
     offsets_z = np.loadtxt(outputPath + "offsets_z.csv", delimiter = ",")
-    
-    digiConfigFile= "/data/atlassmallfiles/users/chri6112/Acts/acts/Examples/Algorithms/Digitization/share/default-smearing-config-telescope.json"
 
-    inputParticlePath = Path("/home/chri6112/Downloads/nmuon_acts_sample_500k.root")
+    rotations_x = np.loadtxt(outputPath + "rotations_x.csv", delimiter=",")
+    rotations_y = np.loadtxt(outputPath + "rotations_y.csv", delimiter=",")
+    rotations_z = np.loadtxt(outputPath + "rotations_z.csv", delimiter=",")
+
+    #inputPath = "/home/chri6112/Downloads/nmuon_acts_sample_200k_"
 
     field = acts.RestrictedBField(acts.Vector3(0* u.T, 0, 1.0 * u.T))
 
-    input_args = list(zip(np.arange(0, 1), offsets_x, offsets_y, offsets_z, itertools.repeat(field), itertools.repeat(digiConfigFile), itertools.repeat(trackingGeometry), itertools.repeat(inputParticlePath), itertools.repeat(rand_), itertools.repeat(outputPath)))
-
+    input_args = list(zip(np.arange(0, 5), offsets_x, offsets_y, -offsets_z, rotations_x, rotations_y, -rotations_z, itertools.repeat(field), itertools.repeat(digiConfigFile), itertools.repeat(trackingGeometry), itertools.repeat(inputParticlePath), itertools.repeat(rand_), itertools.repeat(outputPath)))
     with concurrent.futures.ThreadPoolExecutor(max_workers = 50) as executor:
         executor.map(runACTS, input_args)
-
-    #detector_misal, trackingGeometry_misal, decorators_misal = acts.examples.AlignedTelescopeDetector.create(
-    #        bounds=[500, 1500], positions=[10000, 10500, 11000, 19500, 20000, 20500], binValue=0, offsets=[offsets_z[0], -offsets_y[0], offsets_x[0]], thickness=4,rnd=rand_, 
-    #        sigmaInPlane=0.0 , sigmaOutPlane=0.0 , sigmaOutRot=0.0 , sigmaInRot=0.00 
-    #    )
-
-    #runTruthTrackingKalman(
-    #    trackingGeometry=trackingGeometry,
-    #    trackingGeometry_misal=trackingGeometry_misal,
-    #    field=field,
-    #    digiConfigFile=digiConfigFile,
-    #    outputDir="output/y_2_05_offset_test/2",
-    #    inputParticlePath=inputParticlePath,
-    #).run()
 
 if "__main__" == __name__:
 
