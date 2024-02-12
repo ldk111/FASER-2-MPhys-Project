59999067aab830d197ad9e7ff1d001ba896c25c3
ldk111 <luke.david.kennedy@gmail.com>
2024-02-12 16:18:00 +0000

    added support for multiple input files, returns original sum of squares / absolutes, tries to read dataframe instead of just recreating everytime, multiple different fit options given (needs to be decided which to use)

--- Analysis/evaluation.py
+++ Analysis/evaluation.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
+import joblib
 
 import simulation
 import reconstruction
 
-def Analyse_Run(input_dir, i, offsets_x, offsets_y, offsets_z, rotations_x, rotations_y, rotations_z, initial_guess = np.random.uniform(-0.01, 0.01, 6), bounds = [(-np.pi/8, np.pi/8),(-np.pi/8, np.pi/8),(-np.pi/8, np.pi/8),(-1, 1),(-1, 1),(-1, 1)]):
+def Analyse_Run(input_dir, i, n_inputs, offsets_x, offsets_y, offsets_z, rotations_x, rotations_y, rotations_z, initial_guess = np.random.uniform(-0.01, 0.01, 6), bounds = [(-np.pi/8, np.pi/8),(-np.pi/8, np.pi/8),(-np.pi/8, np.pi/8),(-1, 1),(-1, 1),(-1, 1)], reprop = False):
     """
     Analyses a single run from a given directory, returns the fitted parameters for each plane along with a sum of squares of the residuals between the measured and transformed coordinates.
 
@@ -25,41 +26,83 @@ def Analyse_Run(input_dir, i, offsets_x, offsets_y, offsets_z, rotations_x, rota
     - total_sum_of_residuals_squared: Array with the sum of the residuals squared for each plane.
     """
 
+    #
+
     #Initialise empty arrays to store outputs for each tracking plane in
     total_parameters = []
     total_sum_of_residuals_squared = []
+    total_sum_of_no_squared = []
 
     #Creates a dataframe from the ROOT files from the run
-    df = simulation.Generate_DataFrame_From_ROOT(input_dir, i)
+    try:
+        df = pd.read_csv(input_dir + "df" + str(i) + ".csv")
+    except:
+        df = simulation.Generate_DataFrame_From_ROOT(input_dir, i, n_inputs)
 
     #Creates the offset dataframe
-    df_offsets = simulation.Generate_Predicted_Offset_DataFrame(df)
+    if reprop == False:
+        df_offsets = simulation.Generate_Predicted_Offset_DataFrame(df)
+    else:
+        df_offsets = simulation.Generate_Repropagated_Offset_Dataframe(df, input_dir, i)
 
     #For each of the planes 2 through 6 (1 is fixed to have 0 offset), fit the parameters and retrieve the sum of residuals squared 
     for plane in range(2, 7):
+        if plane < 4:
+            print("Truth transformation parameters: ", np.array([rotations_x[plane-2], rotations_y[plane-2], rotations_z[plane-2], offsets_x[plane-2], offsets_y[plane-2], offsets_z[plane-2]]))
+            parameters, sum_of_residuals_squared, sum_of_no_squared = reconstruction.Fit_Offsets(df_offsets, plane, initial_guess, bounds)
+        else:
+            print("Truth transformation parameters: ", np.array([rotations_x[plane-2], rotations_y[plane-2], rotations_z[plane-2], offsets_x[plane-2], offsets_y[plane-2], offsets_z[plane-2]]))
 
-        parameters, sum_of_residuals_squared = reconstruction.Fit_Offsets(df_offsets, plane, initial_guess, bounds)
+            df_pred_1 = df_offsets
+            df_pred_2 = df_offsets[np.abs((df["PX_TRUTH"] - df["FIT_PX_1"])/df["PX_TRUTH"]) < 0.001]
+            df_pred_3 = df_offsets[(df["P_FIT"] > 1000) & (df["CHI2SUM"] < 25)]
 
+            #if len(df_pred[df_pred["Q_FIT"] == 1]) > len(df_pred[df_pred["Q_FIT"] == -1]):
+            #    df_pred = pd.concat([df_pred[df_pred["Q_FIT"] == -1], df_pred[df_pred["Q_FIT"] == 1].sample(len(df_pred[df_pred["Q_FIT"] == -1]))], ignore_index = True)
+            #else:
+            #    df_pred = pd.concat([df_pred[df_pred["Q_FIT"] == 1], df_pred[df_pred["Q_FIT"] == -1].sample(len(df_pred[df_pred["Q_FIT"] == 1]))], ignore_index = True)
+
+            #print("FULL DATA")
+            #parameters_1, sum_of_residuals_squared, sum_of_no_squared = reconstruction.Fit_Offsets(df_pred_1, plane, initial_guess, bounds)
+            print("HIGH PX RES")
+            parameters_2, sum_of_residuals_squared, sum_of_no_squared = reconstruction.Fit_Offsets(df_pred_2, plane, initial_guess, bounds)
+            #print("HIGH P, LOW CHI2")
+            #parameters_3, sum_of_residuals_squared, sum_of_no_squared = reconstruction.Fit_Offsets(df_pred_3, plane, initial_guess, bounds)
+            #print("FITTED POS MEAN")
+            #parameters_4 = np.array([0,0,0,0,np.mean(df["FIT_Y_HIT_"+str(plane)]-df["LOCAL_Y_HIT_"+str(plane)]),np.mean(df["FIT_Z_HIT_"+str(plane)]-df["LOCAL_Z_HIT_"+str(plane)])])
+            #print(parameters_4)
+            #print("2+4 AVERAGED")
+            #parameters_5 = (parameters_2+parameters_4)/2
+            #print(parameters_5)
+            #print("2+3 AVERAGED")
+            #parameters_6 = (parameters_2+parameters_3)/2
+            #print(parameters_6)
+
+            parameters = parameters_2#np.append([parameters_1],[parameters_2,parameters_3, parameters_4, parameters_5, parameters_6], axis = 0)
+            print(parameters)
         #Add the results of this plane's results to the total results
-        total_parameters.append([parameters])
-        total_sum_of_residuals_squared.append([sum_of_residuals_squared])
+        total_parameters.append(parameters)
+        total_sum_of_residuals_squared.append(sum_of_residuals_squared)
+        total_sum_of_no_squared.append(sum_of_no_squared)
 
-    return total_parameters, total_sum_of_residuals_squared, df, df_offsets
+    return np.array(total_parameters), np.array(total_sum_of_residuals_squared), np.array(total_sum_of_no_squared), df, df_offsets
 
 
-def Analyse_Multiple_Runs(input_dir, n_samples, offsets_x, offsets_y, offsets_z, rotations_x, rotations_y, rotations_z):
+def Analyse_Multiple_Runs(input_dir, n_offsets, n_inputs, offsets_x, offsets_y, offsets_z, rotations_x, rotations_y, rotations_z, initial_guess = np.random.uniform(-0.01, 0.01, 6), bounds = [(-np.pi/8, np.pi/8),(-np.pi/8, np.pi/8),(-np.pi/8, np.pi/8),(-1, 1),(-1, 1),(-1, 1)], reprop = False):
     """
     Analyses a single run from a given directory, returns the fitted parameters for each plane along with a sum of squares of the residuals between the measured and transformed coordinates.
 
     Parameters:
     - input_dir: The path to the main folder of the ACTS run, with folders representing each run inside.
-    - n_samples: The number of runs within input_dir.
+    - n_offsets: The number of runs within input_dir.
     - offsets_x: 2D array with the offsets of the tracking planes along the x axis in mm, such that the global hit = predicted hit + offset (CHECK SIGN HERE).
     - offsets_y: 2D array with the offsets of the tracking planes along the y axis in mm, such that the global hit = predicted hit + offset (CHECK SIGN HERE).
     - offsets_z: 2D array with the offsets of the tracking planes along the z axis in mm, such that the global hit = predicted hit + offset (CHECK SIGN HERE).
     - rotations_x: 2D array with the rotations of the tracking planes along the x axis in radians, currently rotates the hit positions not the detector.
     - rotations_y: 2D array with the rotations of the tracking planes along the y axis in radians, currently rotates the hit positions not the detector.
     - rotations_z: 2D array with the rotations of the tracking planes along the z axis in radians, currently rotates the hit positions not the detector.
+    - initial_guess: Initial guess for the transformation parameters.
+    - bounds: Lower and upper bounds for each of the parameters [tx, ty, tz, dx, dy, dz], can set to (0,0) if you want to eliminate a transformation.
 
     Returns:
     - pred_parameters: 2D array with the fitted parameters for each run and plane within the run.
@@ -70,36 +113,43 @@ def Analyse_Multiple_Runs(input_dir, n_samples, offsets_x, offsets_y, offsets_z,
     #Initialise empty arrays to store outputs for each tracking plane in
     pred_parameters = np.array([])
     total_sum_of_residuals_squared = np.array([])
+    total_sum_of_no_squared = np.array([])
 
     #Not the best way to do this, desired output is a cumulative offset dataframe so multiple runs with different inpus and same offsets (ACTS doesn't like more than 200k events in a run) can be analysed together
     first = True
 
     #For each run within the input_dir
-    for i in range(0, n_samples):
+    for i in range(0, n_offsets):
 
         print("\nANALYSING DATAFRAME: " + str(i) + "\n")
 
         #Retrieve the results from analysing a single run, we do not specify bounds or initial guesses here currently, instead we use the default ones from Analyse_Run
-        pred_parameters_i, total_sum_of_residuals_squared_i, _df, _df_offsets = Analyse_Run(input_dir, i, offsets_y[i], offsets_z[i])
+        pred_parameters_i, total_sum_of_residuals_squared_i, total_sum_of_no_squared_i, _df, _df_offsets = Analyse_Run(input_dir, i, n_inputs, offsets_x[i], offsets_y[i], offsets_z[i], rotations_x[i], rotations_y[i], rotations_z[i], initial_guess, bounds, reprop)
 
         #If this is the first run we store the output offsets dataframe as what is returned
         if first == True:
             first = False
             df_offsets = _df_offsets
+            df = _df
+            pred_parameters = pred_parameters_i
+            total_sum_of_residuals_squared = total_sum_of_residuals_squared_i
+            total_sum_of_no_squared = total_sum_of_no_squared_i
         #If this is not the first run, we append the new offsets dataframe to the current offsets dataframe
         else:
             df_offsets = pd.concat([df_offsets, _df_offsets], ignore_index=True)
-
-        #Add the fitted parameters (sum of residuals squared) onto the total list of fitted parameters (sum of residuals squared)
-        pred_parameters = np.append(pred_parameters, pred_parameters_i)
-        total_sum_of_residuals_squared = np.append(total_sum_of_residuals_squared, total_sum_of_residuals_squared_i)
+            df = pd.concat([df, _df], ignore_index=True)
+            #Add the fitted parameters (sum of residuals squared) onto the total list of fitted parameters (sum of residuals squared)
+            pred_parameters = np.append(pred_parameters, pred_parameters_i, axis = 1)
+            total_sum_of_residuals_squared = np.append(total_sum_of_residuals_squared, total_sum_of_residuals_squared_i)
+            total_sum_of_no_squared = np.append(total_sum_of_no_squared, total_sum_of_no_squared_i)
 
     #This splits the data into n_samples different rows so each row is a single run of ACTS containing the data for 5 tracking planes (not 6 as 1st plane is not fitted since it is fixed as a reference plane)
-    pred_parameters = np.array(np.hsplit(pred_parameters, n_samples))
-    total_sum_of_residuals_squared = np.array(np.hsplit(total_sum_of_residuals_squared, n_samples))
+    pred_parameters = np.array(np.hsplit(pred_parameters, n_offsets))
+    total_sum_of_residuals_squared = np.array(np.hsplit(total_sum_of_residuals_squared, n_offsets))
+    total_sum_of_no_squared = np.array(np.hsplit(total_sum_of_no_squared, n_offsets))
 
     #Save results into the same directory as the main run
-    np.savetxt(input_dir + "pred_parameters.csv", pred_parameters, delimiter = ",")
-    np.savetxt(input_dir + "total_sum_of_residuals_squared.csv", total_sum_of_residuals_squared, delimiter = ",")
+    #np.savetxt(input_dir + "pred_parameters.csv", pred_parameters, delimiter = ",")
+    #np.savetxt(input_dir + "total_sum_of_residuals_squared.csv", total_sum_of_residuals_squared, delimiter = ",")
 
-    return pred_parameters, total_sum_of_residuals_squared, df_offsets
+    return pred_parameters, total_sum_of_residuals_squared, total_sum_of_no_squared, df, df_offsets
