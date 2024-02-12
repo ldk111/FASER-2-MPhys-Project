e80244fe6f63344a15bee8e50544eff03401f4ce
ldk111 <luke.david.kennedy@gmail.com>
2024-02-12 17:30:00 +0000

    added support for another directory level (iteration main folder) and for inputting a path

--- Gaussian Offset Prediction/mkdir.py
+++ Gaussian Offset Prediction/mkdir.py
@@ -1,9 +1,26 @@
 import os
 
-for i in range(0, 10):
-    os.mkdir(str(i))
-    for j in range(0, 13):
+def Generate_Directories(n_iterations, n_offsets, n_inputs, path):
+
+    for i in range(0, n_iterations):
+
         try:
-            os.mkdir(str(i) + "/" + str(j))
+            os.mkdir(path + str(i))
         except FileExistsError:
             print("File Exists")
+
+        for j in range(0, n_offsets):
+
+            try:
+                os.mkdir(path + str(i) + "/" + str(j))
+            except FileExistsError:
+                print("File Exists")
+
+            for k in range(0, n_inputs):
+
+                try:
+                    os.mkdir(path + str(i) + "/" + str(j) + "/" + str(k))
+                except FileExistsError:
+                    print("File Exists")
+
+    return print("Directories successfully created for " + str(n_offsets) + " offsets and " + str(n_inputs) + " inputs.")
